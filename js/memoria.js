6c3deae2ae6d2d789e682c77b40f618b52eab56a
Gabriel García Martínez <UO289097@uniovi.es>
2025-11-03 10:41:59 +0100

    Memoria sin cronómetro

--- js/memoria.js
+++ js/memoria.js
@@ -3,18 +3,84 @@ class Memoria {
         this.tablero_bloqueado = true;
         this.primera_carta = null;
         this.segunda_carta = null;
+        this.reiniciarAtributos();
+        this.barajarCartas();
+        this.tablero_bloqueado = false;
     }
 
-    voltearCarta(card) {
-        card.setAttribute("data-estado", "revelada");
+    voltearCarta(carta) {
+        const estado = carta.getAttribute("data-estado")
+        if (estado === "revelada" || estado === "volteada" || this.tablero_bloqueado) {
+            return;
+        } else {
+            carta.setAttribute("data-estado", "volteada");
+            if (this.primera_carta === null) {
+                this.primera_carta = carta;
+                return;
+            } else {
+                this.segunda_carta = carta;
+                this.comprobarPareja();
+            }
+        }
     }
 
     barajarCartas() {
         const main = document.querySelector("main");
         const cartas = Array.from(document.querySelectorAll("main article"));
 
+        for (let i = cartas.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [cartas[i], cartas[j]] = [cartas[j], cartas[i]];
+        }
 
         cartas.forEach(carta => main.appendChild(carta));
     }
 
+    reiniciarAtributos() {
+        this.tablero_bloqueado = true;
+        this.primera_carta = null;
+        this.segunda_carta = null;
+        this.tablero_bloqueado = false;
+    }
+
+    deshabilitarCartas() {
+        this.primera_carta.setAttribute("data-estado", "revelada");
+        this.segunda_carta.setAttribute("data-estado", "revelada");
+
+        this.comprobarJuego();
+        this.reiniciarAtributos();
+    }
+
+    comprobarJuego() {
+        const cartas = Array.from(document.querySelectorAll("main article"));
+        let reveladas = true;
+        cartas.forEach(carta => {
+            if (carta.getAttribute("data-estado") !== "revelada") {
+                reveladas = false;
+            }
+        });
+    }
+
+    cubrirCartas() {
+        this.tablero_bloqueado = true;
+
+        setTimeout(() => {
+            this.primera_carta.removeAttribute("data-estado");
+            this.segunda_carta.removeAttribute("data-estado");
+            this.reiniciarAtributos();
+        }, 1500);
+
+    }
+
+    comprobarPareja() {
+        const estado1 = this.primera_carta.getAttribute("data-estado");
+        const estado2 = this.segunda_carta.getAttribute("data-estado");
+
+        const img1 = this.primera_carta.querySelector("img").src;
+        const img2 = this.segunda_carta.querySelector("img").src;
+
+        const sonPareja = (estado1 === "volteada" && estado2 === "volteada" && img1 === img2);
+
+        sonPareja ? this.deshabilitarCartas() : this.cubrirCartas();
+    }
 }
